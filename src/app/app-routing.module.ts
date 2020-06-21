a68301946c319bace7571b8c3f079585d6fcfa55
pagarevijayy <pagarevj97@gmail.com>
2020-06-21 12:00:34 +0530

    about page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 const routes: Routes = [
   { path:"home", component: HomepageComponent},
-  { path:"about", component: HomepageComponent},
+  { path:"about", component: AboutComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
